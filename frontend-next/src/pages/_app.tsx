2e6518da83f8363c1a7c872c23b301ee663b434c
RikuOta <otarikuweb@gmail.com>
2022-11-08 22:13:37 +0900

    feat: Mantine をインストール

--- frontend-next/src/pages/_app.tsx
+++ frontend-next/src/pages/_app.tsx
@@ -1,8 +1,36 @@
-import '../styles/globals.css'
+import 'styles/globals.css'
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { MantineProvider, MantineThemeOverride } from '@mantine/core'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
 }
 
-export default MyApp
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+const theme: MantineThemeOverride = {}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page)
+  return (
+    <>
+      <Head>
+        <title>BookApp</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={theme}
+      >
+        {getLayout(<Component {...pageProps} />)}
+      </MantineProvider>
+    </>
+  )
+}
