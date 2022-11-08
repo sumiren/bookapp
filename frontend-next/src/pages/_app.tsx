4e3619498c368543997ad87a693ec7d9169d508d
RikuOta <otarikuweb@gmail.com>
2022-11-09 02:41:00 +0900

    feat: レイアウトを作成

--- frontend-next/src/pages/_app.tsx
+++ frontend-next/src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import 'styles/globals.css'
 import type { ReactElement, ReactNode } from 'react'
+import { useEffect } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { MantineProvider, MantineThemeOverride } from '@mantine/core'
 import resolveConfig from 'tailwindcss/resolveConfig'
 import tailwindConfig from '../../tailwind.config.js'
+import { getCookie } from 'cookies-next'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -34,7 +36,17 @@ const theme: MantineThemeOverride = {
   primaryShade: 5
 }
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+export default function App({ Component, pageProps, router }: AppPropsWithLayout) {
+  // TODO: 認証有無の判定条件を修正
+  // 未認証の場合は /login へリダイレクト
+  // 暫定処理として Cookie の loggedIn を元に判定している
+  useEffect(() => {
+    if (['/login', '/signup'].includes(router.pathname)) return
+    if (!getCookie('loggedIn')) {
+      router.push('/login')
+    }
+  }, [router.pathname])
+
   const getLayout = Component.getLayout ?? ((page) => page)
   return (
     <>
