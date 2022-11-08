a94fd34bd50c2b28cfdb5b9639280c9a0041ecde
RikuOta <otarikuweb@gmail.com>
2022-11-09 01:59:50 +0900

    chore: Tailwind ではなく Mantine の色定義を使用するよう修正

--- frontend-next/src/pages/_app.tsx
+++ frontend-next/src/pages/_app.tsx
@@ -17,7 +17,7 @@ type AppPropsWithLayout = AppProps & {
 
 const resolvedTailwindConfig = resolveConfig(tailwindConfig)
 
-// Tailwind のブレイクポイントを Mantine 用に加工
+// Tailwind のブレイクポイントを Mantine 用に変換
 const tailwindScreensForMantine: {[key: string]: number} = {}
 const tailwindScreens = resolvedTailwindConfig.theme?.screens
 if (tailwindScreens) {
@@ -27,20 +27,9 @@ if (tailwindScreens) {
   }
 }
 
-// Tailwind の色を Mantine 用に変換
-let tailwindColorsForMantine: Record<string, Tuple<string, 10>> = {}
-const tailwindColors = resolvedTailwindConfig.theme?.colors
-if (tailwindColors) {
-  for (const [name, colorsObj] of Object.entries(tailwindColors)) {
-    const colors = Object.values(colorsObj)
-    if (colors.length <= 9) continue
-    tailwindColorsForMantine[name] = colors as Tuple<string, 10>
-  }
-}
-
 const theme: MantineThemeOverride = {
   breakpoints: tailwindScreensForMantine,
-  colors: tailwindColorsForMantine
+  primaryShade: 5
 }
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
--- frontend-next/tailwind.config.js
+++ frontend-next/tailwind.config.js
@@ -1,3 +1,15 @@
+// Mantine の色を Tailwind 用に変換
+//
+// 公式の方法がないため、以下のカラーファイルを使用
+// https://github.com/mantinedev/mantine/blob/master/src/mantine-styles/src/theme/default-colors.ts
+const { DEFAULT_THEME } = require('@mantine/core')
+const mantineColorsForTailwind = {}
+for (const [name, colors] of Object.entries(DEFAULT_THEME.colors)) {
+  mantineColorsForTailwind[name] = colors.reduce((acc, cur, i) => {
+    return { ...acc, [i]: cur }
+  }, {})
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -8,7 +20,7 @@ module.exports = {
     'src/components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    colors: mantineColorsForTailwind
   },
   plugins: [],
   corePlugins: {
